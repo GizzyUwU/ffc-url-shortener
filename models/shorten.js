86f649efb79e5c3876ccfc91607c589c7d990599
NeroGizmo <nerop455@gmail.com>
2022-11-14 13:16:03 +0000

    Updated Model

--- models/shorten.js
+++ models/shorten.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const shortUrlSchema = mongoose.Schema({
         urlid: String,
         url: String,
+        urlpath: String,
+        protocal: String,
 })
 
 module.exports = mongoose.model('shorturl', shortUrlSchema)
